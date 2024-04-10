70367b8449c7aa9325e784f913df00387e5a4979
SaketosBasilis <sdi1800168@di.uoa.gr>
2024-04-10 12:20:55 +0300

    like dislike js

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SearchEnginesUI';
+  liked = 'Like';
+  likeCount = 0;
+  hasLiked = false;
+  AppComponent(){}
+
+
+
+  likeClick() {
+    if (!this.hasLiked) {
+      this.hasLiked = true;
+      this.liked = 'Unlike';
+      this.likeCount += 1;
+    } else {
+      this.hasLiked = false;
+      this.liked = 'Like';
+      this.likeCount -= 1;
+    };
+  }
+
+
+  protected readonly self = self;
 }
