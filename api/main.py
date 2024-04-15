674d19fc6a2ea1f8c6ecc0467058e7fb9e0355d8
Georgios Vidalakis <g.vidalakis@quadible.co.uk>
2024-04-15 15:26:01 +0200

    Implement pagination for the first 10k results.

--- api/main.py
+++ api/main.py
@@ -2,7 +2,14 @@ from typing import Optional
 
 from fastapi import FastAPI
 from elasticsearch import Elasticsearch
+from pydantic_settings import BaseSettings
 
+
+class Settings(BaseSettings):
+    page_size: int = 10
+
+
+settings = Settings()
 app = FastAPI()
 
 
@@ -11,6 +18,8 @@ async def read_items(user_id: Optional[str], query: str, min_publish_time: int, 
     client = Elasticsearch("http://localhost:9200/")
     resp = client.search(
         index="articles",
+        from_=page * settings.page_size,
+        size=settings.page_size,
         body={"query": {"match": {"content": query}}},
     )
     result = []
