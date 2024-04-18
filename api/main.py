ba7019b23e32d467409d0fc77fae8acb37fcc54b
Georgios Vidalakis <g.vidalakis@quadible.co.uk>
2024-04-18 14:24:37 +0200

    Enable CORS.

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from elasticsearch import Elasticsearch
 from pydantic_settings import BaseSettings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class Settings(BaseSettings):
@@ -11,6 +12,14 @@ class Settings(BaseSettings):
 
 settings = Settings()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/search/")
